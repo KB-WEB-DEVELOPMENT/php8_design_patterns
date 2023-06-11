b01dd9c73e810c9803f88d8157ea56ff36f4f50b
Kami Barut-Wanayo <kamibarut@yahoo.com>
2023-06-11 20:57:05 +0200

    Update Player.php

--- src/structural/flyweight/Player.php
+++ src/structural/flyweight/Player.php
@@ -7,15 +7,15 @@ namespace php8_design_patterns\Structural\Flyweight;
 class Player 
 {
 	public function __construct(
-		protected string $playerName,
-		protected string $playerPosition,
-	    protected Team $team
+	   protected string $playerName,
+	   protected string $playerPosition,
+	   protected Team $team
 	){}
 
 	public function printWelcomeMessage(string $playerName,string $playerPosition,Team $team):string 
 	{
 	  $playerTeam = $team->name;
 			
-	   $team->printWelcomeMessage(playerName:$playerName,playerPosition:$playerPosition,playerTeam:(string)playerTeam);
+	  $team->printWelcomeMessage(playerName:$playerName,playerPosition:$playerPosition,playerTeam:(string)playerTeam);
         }		
  }

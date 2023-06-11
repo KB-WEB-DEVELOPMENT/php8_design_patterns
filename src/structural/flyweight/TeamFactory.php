70bd39392e86338cf9e5073a1744b4467c499ee0
Kami Barut-Wanayo <kamibarut@yahoo.com>
2023-06-11 20:54:07 +0200

    Create TeamFactory.php

---
+++ src/structural/flyweight/TeamFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace php8_design_patterns\Structural\Flyweight;
+
+class TeamFactory 
+{
+	public function __construct(
+		private array $playersArray
+	){}
+
+    public function addPlayer(string $playerName,string $playerPosition,Team $team): array
+	{
+		$newPlayer = new Player(playerName:$playerName,playerPosition:$playerPosition,Team:$team);
+		
+        $this->playersArray[] = $newPlayer;
+		
+		return $this->playersArray;
+    }
+
+	public function printWelcomeMessage():string
+	{	
+				$newestPlayer = end($this->playersArray);
+				
+		   		$playerName = (string)$newestPlayer->playerName;
+				$playerPosition = (string)$newestPlayer->playerPosition;
+				$team = $newestPlayer->team;
+				
+				$newestPlayer->printWelcomeMessage(playerName:$playerName,playerPosition:$playerPosition,Team: (string)$team);				
+	}
+ }

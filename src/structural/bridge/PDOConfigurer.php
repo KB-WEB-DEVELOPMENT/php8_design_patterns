cfe4542a7e83030095d81cef659304c94f316341
Kami Barut-Wanayo <kamibarut@yahoo.com>
2023-06-11 20:36:31 +0200

    Create PDOConfigurer.php

---
+++ src/structural/bridge/PDOConfigurer.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace php8_design_patterns\Structural\Bridge;
+
+class PDOConfigurer implements DBConfigurer
+{
+	public function get(array $config): bool
+	{
+
+		$options = [
+			PDO::ATTR_EMULATE_PREPARES => $config["ATT_EMULATE_PREPARES"],
+			PDO::ATTR_ERRMODE => $config["ATTR_ERRMODE"],
+			PDO::ATTR_DEFAULT_FETCH_MODE => $config["ATTR_DEFAULT_FETCH_MODE"],
+		];
+
+		$dsn = 'mysql:host=' . $config["SERVER"] . ';dbname=' . $config["DB"] .  ';charset=' . $config["CHARSET"]; 
+	
+		try {
+	
+			$pdo = new PDO($dsn,$config["USERNAME"],$config["PASSWORD"], $options);
+	
+			return true;
+		
+		} 	else {
+		
+				return false;
+			}	
+	}	
+}

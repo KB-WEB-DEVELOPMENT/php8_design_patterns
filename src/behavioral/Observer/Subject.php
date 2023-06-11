1411fd20057b12cc18ee29093d21674848dc4f24
Kami Barut-Wanayo <kamibarut@yahoo.com>
2023-06-11 19:45:25 +0200

    Create Subject.php

---
+++ src/behavioral/Observer/Subject.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace php8_design_patterns\Behavioral\Observer;
+
+class Subject implements SubjectInterface {
+	
+  private array $observers = [];
+  
+  private string $state;
+
+  public function attach(ObserverInterface $observer):void 
+  {
+	$this->observers[] = $observer;
+  }
+
+  public function detach(ObserverInterface $observer): ?null
+  {
+		
+    $res = is_bool(array_search($observer, $this->observers,true)) == true ?  null : unset($this->observers[$key]);
+				  
+	return $res;
+  }
+
+  public function notify():void
+  {
+    foreach ($this->observers as $observer) {
+      $observer->update($this);
+    }
+  }
+
+  public function setState(string $state):void
+  {
+	$this->state = $state;
+    $this->notify();
+  }
+
+  public function getState():string
+  {
+	return $this->state;
+  }
+  
+}

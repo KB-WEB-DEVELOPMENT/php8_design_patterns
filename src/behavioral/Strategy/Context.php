5992bbe0c4f735951f4fddf614c3057772f42325
Kami Barut-Wanayo <kamibarut@yahoo.com>
2023-06-11 19:52:29 +0200

    Create Context.php

---
+++ src/behavioral/Strategy/Context.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace php8_design_patterns\Behavioral\Strategy;
+
+class Context
+{
+	public function __construct(
+		protected SortingMachine $sortingMachine
+	){}
+
+	public function doSorting(array $unsortedArray): array|bool
+	{
+
+		$sortedArrayOrFalse = $sortingMachine->performSorting($unsortedArray);
+		
+		return $sortedArrayOrFalse;
+	
+	}
+}

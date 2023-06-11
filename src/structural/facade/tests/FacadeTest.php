ce26b1093597ac66023df6497e8ee333bebb14dc
Kami Barut-Wanayo <kamibarut@yahoo.com>
2023-06-11 20:52:53 +0200

    Update FacadeTest.php

--- src/structural/facade/tests/FacadeTest.php
+++ src/structural/facade/tests/FacadeTest.php
@@ -16,28 +16,28 @@ class FacadeTest extends TestCase
 		$mock = $this->createMock(JobFacade::class);
 		
 		$mock->expects($this->once())
-             ->setConstructorArgs([Bank::class,Accommodation::class])
-			 ->method('submitInvoice')
-             ->with('employeedID','IDAX962')
-			 ->with('hoursWorked',48)
-			 ->willReturn('valid invoice');
+                     ->setConstructorArgs([Bank::class,Accommodation::class])
+		     ->method('submitInvoice')
+                      ->with('employeedID','IDAX962')
+		      ->with('hoursWorked',48)
+		      ->willReturn('valid invoice');
 		
-		$this->assertEquals('valid invoice', $mock->submitInvoice('IDAX962',48));
+	        $this->assertEquals('valid invoice', $mock->submitInvoice('IDAX962',48));
 	}
 
-    #[Depends('testCanSubmitInvoice')] 
+        #[Depends('testCanSubmitInvoice')] 
 	public function testCanGetPaid():void
 	{
-		$mock = $this->createMock(JobFacade::class);
+	   $mock = $this->createMock(JobFacade::class);
 		
-		$mock->expects($this->once())
-			 ->setConstructorArgs([Bank::class,Accommodation::class])
-             ->method('getPaid')
-             ->with('employeedID','IDAX962')
-			 ->with('hoursWorked',48)
-			 ->willReturn(true);
+	   $mock->expects($this->once())
+	        ->setConstructorArgs([Bank::class,Accommodation::class])
+                ->method('getPaid')
+                ->with('employeedID','IDAX962')
+		->with('hoursWorked',48)
+		->willReturn(true);
 		
-		$this->assertTrue(true,$mock->getPaid('IDAX962',48));
+	    $this->assertTrue(true,$mock->getPaid('IDAX962',48));
 	}
 	
 	public function testIsPrePaidRequirementMet():void
@@ -45,13 +45,13 @@ class FacadeTest extends TestCase
 		$mock = $this->createMock(JobFacade::class);
 		
 		$mock->expects($this->once())
-             ->setConstructorArgs([Bank::class,Accommodation::class])
-			 ->method('isPrePaidRequirementMet')
-             ->with('passwordID','JOLKI927')
-			 ->with('address','25 Jackson Street')
-             ->with('contactInfos',['landlineNum' => 123456, 'cellNum' => 35788, 'email' => 'test@gmail.com'])
-			 ->with('amount',1200)			 			 
-			 ->willReturn(true);
+                     ->setConstructorArgs([Bank::class,Accommodation::class])
+		     ->method('isPrePaidRequirementMet')
+                     ->with('passwordID','JOLKI927')
+		     ->with('address','25 Jackson Street')
+                      ->with('contactInfos',['landlineNum' => 123456, 'cellNum' => 35788, 'email' => 'test@gmail.com'])
+		      ->with('amount',1200)			 			 
+		      ->willReturn(true);
 		
 		$this->assertTrue(true,$mock->isPrePaidRequirementMet('JOLKI927','25 Jackson Street',['landlineNum' => 123456, 'cellNum' => 35788, 'email' => 'test@gmail.com'],1200));	
 	}	

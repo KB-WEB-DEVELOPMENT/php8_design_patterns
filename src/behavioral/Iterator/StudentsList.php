a9c3ac852ee027815063e62ef4d619963eb394b5
Kami Barut-Wanayo <kamibarut@yahoo.com>
2023-06-11 19:31:45 +0200

    Add files via upload

---
+++ src/behavioral/Iterator/StudentsList.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace php8_design_patterns\Behavioral\Iterator;
+
+class StudentsList {
+	
+    private array $studentsArray = [];
+    
+	public function getStudent(int $arrayIndex): array|bool
+	{      
+	     $student =  is_array($this->studentsArray[$arrayIndex]) ? $this->studentsArray[$arrayIndex] : false;
+	  
+	     return $student;		  
+    }
+    
+	public function addStudent(Student $newStudent): bool
+	{
+		$arrayIndex = key(end($this->studentsArray)) + 1;
+	  
+		$this->studentsArray[$arrayIndex] = $newStudent;
+	  
+		$res = ($this->studentsArray[$arrayIndex] == end($this->studentsArray)) ? true : false;
+	  
+		return $res;
+    }
+    
+	public function removeStudent(Student $studentToDelete): bool
+	{
+		$studentName = $studentToDelete->name;
+
+		$arrayIndex =  array_search($studentName, array_column($this->studentsArray,'name'));
+
+        if (!is_bool($arrayIndex)) {
+			
+			unset($this->studentsArray[$arrayIndex]);
+			
+			$this->studentsArray = array_values($this->studentsArray); 
+		
+			return true;		
+		}	
+
+		return false;
+    }
+	
+	public function countStudents(): int
+	{
+		return count($this->studentsArray);
+	}
+}
---
+++ src/behavioral/Iterator/StudentsListBackwardIterator.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace php8_design_patterns\Behavioral\Iterator;
+
+class StudentsListBackwardIterator extends StudentsListForwardIterator
+{	
+    protected int $currentArrayIndex;
+
+    public function __construct(
+		protected StudentsList $studentsList	
+	){}
+	
+	public function getCurrentStudent(): object|null
+	{
+        		
+		$res = array_key_exists($this->currentArrayIndex,$this->studentsList) ? $this->studentsList[$this->currentArrayIndex] : null;
+				   
+		return $res;               		
+	}	
+
+	public function getNextStudent(): object|null
+	{
+		 $res =  array_key_exists(--$this->currentArrayIndex,$this->studentsList) ? $this->studentsList[--$this->currentArrayIndex] : null;
+	
+		 return $res;
+	}
+
+}
+

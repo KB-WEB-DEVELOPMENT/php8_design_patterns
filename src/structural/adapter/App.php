c930c3161f04df09cc42516be2a46794df8c41e2
Kami Barut-Wanayo <kamibarut@yahoo.com>
2023-06-11 20:32:00 +0200

    Create App.php

---
+++ src/structural/adapter/App.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace php8_design_patterns\Structural\Adapter;
+
+class App 
+{
+    
+  public function __construct() 
+  {
+	  
+	private object $windowsCommandsAnalyzerObj;
+  }
+	
+  public function getCommand(string $command):string 
+  {  
+	  $this->windowsCommandsAnalyzerObj = new WindowsCommandsAnalyzer();
+	
+	  $commandInfos = $this->windowsCommandsAnalyzerObj->getWindowsCommand(command:$command);
+    
+	  return $commandInfos;
+  }	
+ 
+  public function setConverter(WindowsToLinuxAdapter $adapter):void 
+  {
+	  $this->windowsCommandsAnalyzerObj = $adapter;
+  }
+ 	
+  public function convertCommand(string $command):void 
+  {
+	  $this->windowsCommandsAnalyzerObj->convert(command:$command);
+  }
+			
+}

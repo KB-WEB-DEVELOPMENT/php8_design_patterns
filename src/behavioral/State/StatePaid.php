1bd3ae429d518ae9895479af24529f1af0e26375
Kami Barut-Wanayo <kamibarut@yahoo.com>
2023-06-11 19:48:55 +0200

    Create StatePaid.php

---
+++ src/behavioral/State/StatePaid.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace php8_design_patterns\Behavioral\State;
+
+class StatePaid implements State
+{	
+	public function __construct(
+		private string $borrowerCodeId,
+		private array $loanStatus
+	){}
+		
+	public function doNext(LoanContext $context):void
+	{
+		$context->setState(new StateClosed($this->borrowerCodeId,$this->loanStatus));
+	}
+
+	public function printLoanStatus(): string
+	{
+		return 'BORROWER CODE ID: #' . $this->borrowerCodeId . ' LOAN STATUS: ' . $this->loanStatus['step3of4'];
+	}
+}

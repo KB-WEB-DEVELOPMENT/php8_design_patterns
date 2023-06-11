7c84e505ce35b656319f73add3a50a8431f31836
Kami Barut-Wanayo <kamibarut@yahoo.com>
2023-06-11 21:05:36 +0200

    Update FilesReaderLib.php

--- src/structural/proxy/FilesReaderLib.php
+++ src/structural/proxy/FilesReaderLib.php
@@ -6,8 +6,8 @@ namespace php8_design_patterns\Structural\Proxy;
 
 interface  FilesReaderLib
 {
-	public function isLocalFile():  bool;
-	public function isReadableFile(): bool;
-	public function getFileInfos(): array;
-	public function copyRemoteFile(): bool;
+   public function isLocalFile():  bool;
+   public function isReadableFile(): bool;
+   public function getFileInfos(): array;
+   public function copyRemoteFile(): bool;
 }

c11f378689b9fd80833d554ade18e69a311d3fef
Kami Barut-Wanayo <kamibarut@yahoo.com>
2023-06-11 20:57:45 +0200

    Update Team.php

--- src/structural/flyweight/Team.php
+++ src/structural/flyweight/Team.php
@@ -5,13 +5,12 @@ namespace php8_design_patterns\Structural\Flyweight;
 
 class Team 
 {	
-	public function __construct(
-		protected string $teamName
-	){}
+    public function __construct(
+      protected string $teamName
+    ){}
 
-	public function printWelcomeMessage(string $playerName,string $playerPosition,string $playerTeam): string 
-	{
-		return 'Welcome to the ' . $playerPosition . '&nbsp;' . $playerName . 'at ' . $playerTeam;   
-    }
-		
+    public function printWelcomeMessage(string $playerName,string $playerPosition,string $playerTeam): string 
+    {
+      return 'Welcome to the ' . $playerPosition . '&nbsp;' . $playerName . 'at ' . $playerTeam;   
+    }		
  }

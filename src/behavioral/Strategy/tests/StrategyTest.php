13ae1a1e8a394a85b062dcbc7ad5fd7d8ceb79b7
Kami Barut-Wanayo <kamibarut@yahoo.com>
2023-06-14 19:13:43 +0200

    Add files via upload

--- src/behavioral/Strategy/tests/StrategyTest.php
+++ src/behavioral/Strategy/tests/StrategyTest.php
@@ -12,73 +12,78 @@ use PHPUnit\Framework\TestCase;
 class StrategyTest extends TestCase
 {
 	public function listSortingDataProvider(): array
-        {
-        	return [
-			 'unsorted' => ['Finland', 'Spain', 'Burundi', 'Canada', 'USA'],
-		 	'sorted'    =>  ['Burundi', 'Canada', 'Finland', 'Spain', 'USA']
-	       ];
-        }
+    {
+        return [
+				'unsorted' => 
+						['Finland', 'Spain', 'Burundi', 'Canada', 'USA'],
+				'sorted'  =>		
+						['Burundi', 'Canada', 'Finland', 'Spain', 'USA']
+				];
+    }
 	
 	public function mapSortingDataProvider(): array
-        {
-        	return [
-			 'unsorted' =>	[7 => 'Croatia', 2 => 'Austria', 13 => 'Belgium', 1 => 'USA'],
-			 'sorted by key' => [1 => 'USA', 2 => 'Austria',  7 => 'Croatia', 13 => 'Belgium']
-	        ];
+    {
+        return [
+				'unsorted' =>	
+						[7 => 'Croatia', 2 => 'Austria', 13 => 'Belgium', 1 => 'USA'],
+				'sorted by key' =>	
+						[1 => 'USA', 2 => 'Austria',  7 => 'Croatia', 13 => 'Belgium']
+			   ]
 	}	
 		
-       /**
-       * @dataProvider listSortingDataProvider
-       */
-       public function testCanSortByList(array $unsortedArray,array $expectedSortedArray):void
-       {
-        	$list = new ListSortingMachine('asc');
+	/**
+     * @dataProvider listSortingDataProvider
+    */
+    public function testCanSortByList(array $unsortedArray,array $expectedSortedArray):void
+    {
+        $list = new ListSortingMachine(sortDir:'asc');
 		
-		$contextObj = new Context($list);
+		$contextObj = new Context(sortingMachine:$list);
 		
 		$sortedArr = $contextObj->performSorting(unsortedArray:$unsortedArray);
 		
 		$this->assertSame($expectedSortedArray,$sortedArr);
-       }
+    }
 	
 	/**
-        * @dataProvider mapSortingDataProvider
-        */
-       public function testCanSortByMap(array $unsortedArray,array $expectedSortedArray):void
-       {
-          $map = new MapSortingMachine('key','asc');
+     * @dataProvider mapSortingDataProvider
+    */
+    public function testCanSortByMap(array $unsortedArray,array $expectedSortedArray):void
+    {
+      $map = new MapSortingMachine(sortBy:'key',sortDir:'asc');
 	  
-	  $contextObj = new Context($map);
+	  $contextObj = new Context(sortingMachine:$map);
 
 	  $sortedArr = $contextObj->performSorting(unsortedArray:$unsortedArray);
 	  
 	  $this->assertSame($expectedSortedArray,$sortedArr);
-       }
+    }
 	
-      public function testIsListSortingMachineConfigWrong():void
-      {
-      	$unsorted = ['Finland','Spain','Burundi','Canada','USA'];
+    public function testIsListSortingMachineConfigWrong():void
+    {
+		$unsorted = ['Finland','Spain','Burundi','Canada','USA'];
 		
-        $list = new ListSortingMachine('left');
+		$list = new ListSortingMachine(sortDir:'left');
 
-        $contextObj = new Context($list);
+		$contextObj = new Context(sortingMachine:$list);
 		
-	$res = $contextObj->performSorting(unsortedArray:$unsorted);
+		$res = $contextObj->performSorting(unsortedArray:$unsorted);
 		
-	$this->assertSame(false,$res);	
+		$this->assertSame(false,$res);
+		
     }	
 	
     public function testIsMapSortingMachineConfigWrong():void
     {
-       $unsorted = [7 => 'Croatia', 2 => 'Austria', 13 => 'Belgium', 1 => 'USA'];
+		$unsorted = [7 => 'Croatia', 2 => 'Austria', 13 => 'Belgium', 1 => 'USA'];
 		
-       $map = new MapSortingMachine('key','right');
+		$map = new MapSortingMachine(sortBy:'key',sortDir:'right');
 
-       $contextObj = new Context($map);
+		$contextObj = new Context(sortingMachine:$map);
 		
-       $res = $contextObj->performSorting(unsortedArray:$unsorted);
+		$res = $contextObj->performSorting(unsortedArray:$unsorted);
 		
-       $this->assertSame(false,$res);
+		$this->assertSame(false,$res);
     }
 	
 }

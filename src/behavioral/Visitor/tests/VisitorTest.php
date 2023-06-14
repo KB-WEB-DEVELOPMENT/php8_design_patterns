1e27aa49fcc543a03f377f4b21c2fac595e59e0c
Kami Barut-Wanayo <kamibarut@yahoo.com>
2023-06-14 19:21:39 +0200

    Update VisitorTest.php

--- src/behavioral/Visitor/tests/VisitorTest.php
+++ src/behavioral/Visitor/tests/VisitorTest.php
@@ -17,35 +17,33 @@ class VisitorTest extends TestCase
     public function testCanCalculateShapeProps():void
     {   
 	    
-		$shapesPropsCalculator = new ShapesPropertiesCalculator();
+	$shapesPropsCalculator = new ShapesPropertiesCalculator();
 		
-		$diskObj = new Disk(radius:5);
-		$rectObj = new Rectangle(width:4,height:3);
-		$triangleObj = new Triangle(side1:3,side2:4,side3:6);
-		$squareObj = new Square(side:10);
+	$diskObj = new Disk(radius:5);
+	$rectObj = new Rectangle(width:4,height:3);
+	$triangleObj = new Triangle(side1:3,side2:4,side3:6);
+	$squareObj = new Square(side:10);
 				
-		$geoShapesObjArr = [];
+	$geoShapesObjArr = [];
 		
-		$geoShapesObjArr[] = [$diskObj,$rectObj,$triangleObj,$squareObj];
+	$geoShapesObjArr[] = [$diskObj,$rectObj,$triangleObj,$squareObj];
 
-		$resArray = [];
+	$resArray = [];
 	
-		foreach($geoShapesObjArr as $shapeObj) {
-			
-			$resArray[] = $shapeObj.accept(visitor:$shapesPropsCalculator);
-
-		}	
+	foreach($geoShapesObjArr as $shapeObj) {
+		$resArray[] = $shapeObj.accept(visitor:$shapesPropsCalculator);
+	}	
 		
-		$expArray = [];
+	$expArray = [];
 		
-		$expArray = [
-			'Shape:Disk,Area=78.54 unit length squared,Perimeter=31.42 unit length',
-			'Shape:Rectangle,Area=12 unit length squared,Perimeter=7 unit length',
-			'Shape:Triangle,Area=5.33 unit length squared,Perimeter=13 unit length',
-			'Shape:Square,Area=100 unit length squared,Perimeter=40 unit length',	
-		];
+	$expArray = [
+		'Shape:Disk,Area=78.54 unit length squared,Perimeter=31.42 unit length',
+		'Shape:Rectangle,Area=12 unit length squared,Perimeter=7 unit length',
+		'Shape:Triangle,Area=5.33 unit length squared,Perimeter=13 unit length',
+		'Shape:Square,Area=100 unit length squared,Perimeter=40 unit length',	
+	];
 	
-		$this->assertSame($expArray,$resArray);
-	}
+	$this->assertSame($expArray,$resArray);
+   }
 
 }

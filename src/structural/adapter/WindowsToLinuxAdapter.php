7f518806819906114d732190ff311a7460d96ea9
Kami Barut-Wanayo <kamibarut@yahoo.com>
2023-06-19 09:54:44 +0200

    Update WindowsToLinuxAdapter.php

--- src/structural/adapter/WindowsToLinuxAdapter.php
+++ src/structural/adapter/WindowsToLinuxAdapter.php
@@ -5,12 +5,10 @@ namespace php8_design_patterns\Structural\Adapter;
 
 class WindowsToLinuxAdapter extends LinuxCommandsAnalyzer {
  
-    public function __construct() 
-    {
-        private LinuxCommandsAnalyzer $linuxCommandsAnalyzer;
-    }
- 
-
+    public function __construct(
+     private LinuxCommandsAnalyzer $linuxCommandsAnalyzer,
+    ){}
+    
     public  function convert(string $command):void 
     {
         $this->linuxCommandsAnalyzer->getLinuxCommand(command:$command);        

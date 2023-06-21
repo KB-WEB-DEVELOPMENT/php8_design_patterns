a128944682949b12fcc29938e9c8e5d49f14ee49
Kami Barut-Wanayo <kamibarut@yahoo.com>
2023-06-21 15:26:14 +0200

    Update Observer.php

--- src/behavioral/Observer/Observer.php
+++ src/behavioral/Observer/Observer.php
@@ -3,14 +3,11 @@
 namespace php8_design_patterns\Behavioral\Observer;
 
 class Observer implements ObserverInterface {
+
+  public function __construct(
+	private int $id
+  ){}
 	
-  private int $id;
-
-  public function __construct($id)
-  {
-    $this->id = $id;
-  }
-
   public function update(SubjectInterface $subject):string
   {
     echo 'Observer {$this->id} notified with new state: {$subject->getState()}.' . '<br/>';

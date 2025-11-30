09dabd90b57d01a165232889722d84aae55aa4e2
Kami Barut-Wanayo <kamibarut@yahoo.com>
2025-11-30 13:23:08 +0100

    Update HtmlElementPrototype.php

--- src/creational/Prototype/HtmlElementPrototype.php
+++ src/creational/Prototype/HtmlElementPrototype.php
@@ -6,10 +6,10 @@ namespace php8_design_patterns\Creational\Prototype;
 abstract class HtmlElementPrototype
 {
 	public function __construct( 
-           public string $elementId,
-           public string $elementName,
-	   public array  $elementConfig,
-        ) {}
+    	public string $elementId,
+        public string $elementName,
+	    public array  $elementConfig,
+    ){}
 
 	abstract public function __clone();
 

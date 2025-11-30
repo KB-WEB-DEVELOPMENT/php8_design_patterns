0bd08f5cd85450cc05753da57bd11cf89f97982c
Kami Barut-Wanayo <kamibarut@yahoo.com>
2025-11-30 13:24:41 +0100

    Update SelectHtmlElementPrototype.php

--- src/creational/Prototype/SelectHtmlElementPrototype.php
+++ src/creational/Prototype/SelectHtmlElementPrototype.php
@@ -6,12 +6,11 @@ namespace php8_design_patterns\Creational\Prototype;
 
 class SelectHtmlElementPrototype extends HtmlElementPrototype
 {
-		
 	public function __construct(
-          public string $elementId,
-          public string $elementName, 
-	  public array $elementConfig,
-	) {}
+    	public string $elementId,
+        public string $elementName, 
+	  	public array $elementConfig,
+	){}
 
 	public function __clone();
 
@@ -33,13 +32,13 @@ class SelectHtmlElementPrototype extends HtmlElementPrototype
 			  $autofocus = $this->elementConfig["autofocus"]  == true ? "autofocus" : null;	
 			  $disabled =  $this->elementConfig["disabled"]  == true ? "disabled" : null;	 	 	
 			  $multiple =  $this->elementConfig["multiple"]  == true ? "mutiple" : null;	 		  	
-                          $required = $this->elementConfig["required"]  == true ? "required" : null;	 
-                          $size = (is_int($this->elementConfig["size"]) && $this->elementConfig["rows"] > 0) ? $this->elementConfig["size"] : 50;
+              $required = $this->elementConfig["required"]  == true ? "required" : null;	 
+              $size = (is_int($this->elementConfig["size"]) && $this->elementConfig["rows"] > 0) ? $this->elementConfig["size"] : 50;
 			  
 			  $htmlCode = '<select name="' . $this->elementName . '" id="' . $this->elementId . '"';
 			  
 			  $htmlCode .= '&nbsp;'.$autofocus;
-                          $htmlCode .= '&nbsp;'.$disabled;
+              $htmlCode .= '&nbsp;'.$disabled;
 			  $htmlCode .= '&nbsp;'.$mutiple;
 			  $htmlCode .= '&nbsp;'.$required;			  
 			  $htmlCode .= '&nbsp;size="' . $size . '">';
@@ -57,13 +56,12 @@ class SelectHtmlElementPrototype extends HtmlElementPrototype
 		  } else {
 			  
 			 $htmlCode = '<select name="' . $this->elementName . '" id="' . $this->elementId . '">';
-                         $htmlCode .= '<option value="valueName1">Value1</option>';
+             $htmlCode .= '<option value="valueName1">Value1</option>';
 			 $htmlCode .= '<option value="valueName2">Value2</option>';
 			 $htmlCode .= '<option value="valueName3">Value3</option>';
-                         $htmlCode .= '</select>';
+             $htmlCode .= '</select>';
   
 			 return $htmlCode; 
 		  }			
 	}
-
 }

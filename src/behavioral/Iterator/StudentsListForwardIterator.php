64a602416c199dbdb825da750ae0b78654d6cf99
Kami Barut-Wanayo <kamibarut@yahoo.com>
2025-11-30 13:15:56 +0100

    Update StudentsListForwardIterator.php

--- src/behavioral/Iterator/StudentsListForwardIterator.php
+++ src/behavioral/Iterator/StudentsListForwardIterator.php
@@ -13,9 +13,8 @@ class StudentsListForwardIterator
 	){}
 	
 	public function getCurrentStudent(): object|null
-	{
-        
-                $res = array_key_exists($this->currentArrayIndex,$this->studentsList) ? $this->studentsList[$this->currentArrayIndex] : null;
+	{      
+        $res = array_key_exists($this->currentArrayIndex,$this->studentsList) ? $this->studentsList[$this->currentArrayIndex] : null;
 				   
 		return $res;               		
 	}	
